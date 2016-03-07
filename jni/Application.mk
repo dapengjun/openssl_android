7c90ae0689d89791dec1279a7396f3306177e752
lixiangjun <lixiangjun@sursen.net>
2016-03-07 15:23:41 +0800

    add support for arm64-v8a

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
-APP_ABI := armeabi armeabi-v7a x86 mips
+APP_ABI := armeabi armeabi-v7a arm64-v8a x86 mips
 #APP_ABI := armeabi armeabi-v7a
